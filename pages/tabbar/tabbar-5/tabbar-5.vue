99cbecc6b5226ad831fa5a35ed35dbad7a772e54
houMao1997 <1328075766@qq.com>
2020-01-03 11:28:40 +0800

    个人中心

--- pages/tabbar/tabbar-5/tabbar-5.vue
+++ pages/tabbar/tabbar-5/tabbar-5.vue
@@ -1,27 +1,25 @@
 <template>
 	<view class="content">
-		<view style="width: 100%;height:300upx;background: #7CB0FB;"></view>
+		<view style="width: 100%;height:300upx; background-image: linear-gradient(to right,#7BAFFA, #8FC1FE);"></view>
 		<view class="personal">
 			<image style="width: 200upx;height:200upx;border-radius: 50%;margin-top: -15%;border: 5px #fff solid;" src="../../../static/img/kc1.jpg"></image>
 			<view style="font-size: 40upx;color: #90B7FF;">Lyy</view>
 			<view  style="font-size: 30upx;color: #ccc;margin-top: 15upx;">一个努力的英语学习者</view>
 			<view class="adlist">
 				<view class="listitem listitem1">
-					<view><image style="width:100upx;height:100upx;" src="../../../static/img/my9.png"></image></view>
-					<view style="font-size:25rpx;">数据分析</view>
+					<view><image style="width:100%;height:100upx;" src="../../../static/img/my9.png"></image></view>
+					<view style="font-size:25rpx;color: #959595;">数据分析</view>
 				</view>
 				<view class="listitem listitem2">
-					<view><image style="width:100upx;height:100upx;" src="../../../static/img/my10.png"></image></view>
-					<view style="font-size:25rpx;">考试记录</view>
+					<view><image style="width:100%;height:100upx;" src="../../../static/img/my10.png"></image></view>
+					<view style="font-size:25rpx;color: #959595;">考试记录</view>
 				</view>
 				<view class="listitem listitem3">
-					<view><image style="width:100upx;height:100upx;" src="../../../static/img/my11.png"></image></view>
-					<view style="font-size:25rpx;">图表统计</view>
+					<view><image style="width:100%;height:100upx;" src="../../../static/img/my11.png"></image></view>
+					<view style="font-size:25rpx;color: #959595;">图表统计</view>
 				</view>
 			</view>
 		</view>
-		
-<<<<<<< HEAD
 		<!--我的收藏  -->
 		
 			<!-- <view class="listitem listitem1 item">
@@ -61,70 +59,77 @@
 				<view style="font-size:30rpx;">图表统计</view>
 			</view> -->
 	
-=======
+
 		<view>
 			<view class="zblist" style="margin-top:30upx;">
-				<view class="zblb">
-					<view class="zblblist">
-						<view><image  class="zbimg"  src="../../../static/img/my1.png"></image></view>
-						<view class="title1" >我的收藏</view>
-					</view>
-					<view class="zblblist">
-						<view><image  class="zbimg"  src="../../../static/img/my2.png"></image></view>
-						<view class="title1" >历史考试</view>
-					</view>
-					<view class="zblblist">
-						<view><image  class="zbimg"  src="../../../static/img/my3.png"></image></view>
-						<view class="title1" >我的下载</view>
-					</view>
-					<view class="zblblist">
-						<view><image  class="zbimg"  src="../../../static/img/my4.png"></image></view>
-						<view class="title1" >我的预约</view>
-					</view>
-					<view class="zblblist">
-						<view><image  class="zbimg"  src="../../../static/img/my5.png"></image></view>
-						<view class="title1" >在线直播</view>
-					</view>
-					<view class="zblblist">
-						<view><image  class="zbimg"  src="../../../static/img/my6.png"></image></view>
-						<view class="title1" >我的钱包</view>
-					</view>
-					<view class="zblblist">
-						<view><image  class="zbimg"  src="../../../static/img/my7.png"></image></view>
-						<view class="title1" >我的购买</view>
-					</view>
-					<view class="zblblist">
-						<view><image  class="zbimg"  src="../../../static/img/my8.png"></image></view>
-						<view class="title1" >主题皮肤</view>
-					</view>
-					<view class="zblblist">
-						<view><image  class="zbimg"  src=""></image></view>
-						<view class="title1" ></view>
-					</view>
+				<view class="zblb" >
+					<uni-grid :column="3" :show-border="false"  :square="true" style="text-align: center;margin: 20px auto;">
+					    <uni-grid-item>
+					       <view><image  class="zbimg"  src="../../../static/img/my1.png"></image></view>
+					       <view class="title1" >我的收藏</view>
+					    </uni-grid-item>
+					    <uni-grid-item>
+					        <view><image  class="zbimg"  src="../../../static/img/my2.png"></image></view>
+					        <view class="title1" >历史考试</view>
+					    </uni-grid-item>
+					    <uni-grid-item>
+					       <view><image  class="zbimg"  src="../../../static/img/my3.png"></image></view>
+					       <view class="title1" >我的下载</view>
+					    </uni-grid-item>
+					    <uni-grid-item>
+					        <view><image  class="zbimg"  src="../../../static/img/my4.png"></image></view>
+					        <view class="title1" >我的预约</view>
+					    </uni-grid-item>
+					    <uni-grid-item>
+					        <view><image  class="zbimg"  src="../../../static/img/my5.png"></image></view>
+					        <view class="title1" >在线直播</view>
+					    </uni-grid-item>
+					    <uni-grid-item>
+					        <view><image  class="zbimg"  src="../../../static/img/my6.png"></image></view>
+					        <view class="title1" >我的钱包</view>
+					    </uni-grid-item>
+						<uni-grid-item>
+						    <view><image  class="zbimg"  src="../../../static/img/my7.png"></image></view>
+						    <view class="title1" >我的购买</view>
+						</uni-grid-item>
+						<uni-grid-item>
+						    <view><image  class="zbimg"  src="../../../static/img/my8.png"></image></view>
+						    <view class="title1" >主题皮肤</view>
+						</uni-grid-item>
+						<!-- <uni-grid-item>
+						    <view><image  class="zbimg"  src="../../../static/img/my1.png"></image></view>
+						    <view class="title1" >我的收藏</view>
+						</uni-grid-item> -->
+					</uni-grid>
+					
+					
 				</view>
 			</view>
+
 		</view>
 		
 		<view class="adlist1">
 			<view class="listitem listitem1">
 				<view><image style="width:100upx;height:100upx;" src="../../../static/img/my12.png"></image></view>
-				<view style="font-size:25rpx;">意见反馈</view>
+				<view style="font-size:30rpx;">意见反馈</view>
 			</view>
 			<view class="listitem listitem2">
 				<view><image style="width:100upx;height:100upx;" src="../../../static/img/my13.png"></image></view>
-				<view style="font-size:25rpx;">在线客服</view>
+				<view style="font-size:30rpx;">在线客服</view>
 			</view>
 			<view class="listitem listitem3">
 				<view><image style="width:100upx;height:100upx;" src="../../../static/img/my14.png"></image></view>
-				<view style="font-size:25rpx;">商务合作</view>
+				<view style="font-size:30rpx;">商务合作</view>
 			</view>
 		</view>
->>>>>>> 4458c711bbf68a006ba26425eff773e58b81d1b6
 	</view>
 </template>
 
 <script>
+	import uniGrid from "@/components/uni-grid/uni-grid.vue"
+	import uniGridItem from "@/components/uni-grid-item/uni-grid-item.vue"
 	export default {
+		components: {uniGrid,uniGridItem},
 		data() {
 			return {
 				title: 'Hello'
@@ -171,7 +176,7 @@
 		padding-bottom: 30upx;
 		
 	}
-<<<<<<< HEAD
+
 	.listitem{
 		text-align: center;
 		margin: 0 auto;
@@ -189,7 +194,7 @@
 		/* height:200upx ; */
 		background-color: pink;
 	}
-=======
+
 .adlist{
 	width: 90%;
 	display: flex;
@@ -227,7 +232,7 @@
 	box-shadow:  2px 2px 2px 1px #ccc;;
 	border: 1px #ccc solid;
 }
-.zblb{
+/* .zblb{
 	display: flex;
 	justify-content: space-around;
 	flex-wrap: wrap;
@@ -239,24 +244,24 @@
 }
 .zblblist{
 	width: 30%;
-	/* text-align: center; */
 	margin-top: 30upx;
-}
+} */
 .title1{
-	font-size: 26upx;
-	color: #007AFF;
+	font-size: 30upx;
+	color: #505050;
 	height: 40upx;
 	line-height: 40upx;
 	margin-top: 10upx;
-	text-align: left;
+	text-align: center; 
 }
 .zbimg{
-	display: block;
-	width:50%;
-	height: 100upx;
-	border-radius: 10px;
+ 	display: block;
+	width:100upx;
+	height: 100upx; 
+	/* border-radius: 10px;
+	text-align: right; */
+	text-align: center;
+	margin-left: 26%;
 } 
 
-
->>>>>>> 4458c711bbf68a006ba26425eff773e58b81d1b6
 </style>
